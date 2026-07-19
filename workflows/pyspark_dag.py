6eb3932da2ac56d454aae2069b09e459fc5bd4d4
dwarakesh1121-netizen <dwarakesh1121@gmail.com>
2026-07-19 22:01:25 +0530

    Update pyspark_dag.py

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -10,10 +10,10 @@ from airflow.providers.google.cloud.operators.dataproc import (
 )
 
 # define the variables
-PROJECT_ID = "avd-databricks-demo"
+PROJECT_ID = "platinum-hour-502315-q5"
 REGION = "us-east1"
-CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-a649b781-bucket" ## change this 
+CLUSTER_NAME = "bojack"
+COMPOSER_BUCKET = " us-central1-dag-fd71231c-bucket" ## change this 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/hospitalA_mysqlToLanding.py"
 PYSPARK_JOB_1 = {
